e3df88f39a7e7544a005277f63140f31e4d83106
Simon Parkyn <s.a.j.parkyn@brighton.ac.uk>
2026-08-10 16:13:22 +0100

    Exercise 4 solution - render CodeSnippet for pre tags in MDX

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import CodeSnippet from "@/components/CodeSnippet";
 
 import BlogHero from "@/components/BlogHero";
 import { BLOG_TITLE } from "@/constants";
@@ -25,7 +26,12 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet,
+          }}
+        />
       </div>
     </article>
   );
