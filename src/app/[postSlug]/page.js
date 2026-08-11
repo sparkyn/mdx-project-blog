50331f4c860cbc9b0211c53845f08d0c9e11b487
Simon Parkyn <s.a.j.parkyn@brighton.ac.uk>
2026-08-11 14:32:03 +0100

    Exercise 5A solution - my working simple solution replaced with official solution with Lazy Loading moved to component's index.js, and component map created as separate constant in helper file

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import CodeSnippet from "@/components/CodeSnippet";
 
 import BlogHero from "@/components/BlogHero";
 import { BLOG_TITLE } from "@/constants";
 
 import styles from "./postSlug.module.css";
+import COMPONENT_MAP from "@/helpers/mdx-components";
 
 export async function generateMetadata({ params }) {
   const { frontmatter } = await loadBlogPost(params.postSlug);
@@ -26,12 +26,7 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote
-          source={content}
-          components={{
-            pre: CodeSnippet,
-          }}
-        />
+        <MDXRemote source={content} components={COMPONENT_MAP} />
       </div>
     </article>
   );
